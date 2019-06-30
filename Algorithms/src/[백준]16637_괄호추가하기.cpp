ecd8f41afacb89fb00fa89b0b45d611e6f5c89a4
wokickic <lsw911563@nate.com>
2019-06-30 17:54:03 +0900

    백준

--- Algorithms/src/[백준]16637_괄호추가하기.cpp
+++ Algorithms/src/[백준]16637_괄호추가하기.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 #define MAX 10
+#define max(a,b) ((a > b) ? (a) : (b))
 using namespace std;
 
 int n;
 int num[MAX];
 char op[MAX-1];
 
-int cal(){
-    
+int cal(int idx){
+    int ret = num[idx - 1];
+    for(int i = idx; i < n; i+=2){
+        if(op[i] == '+') ret += num[i + 1];
+        else if(op[i] == '-') ret -= num[i + 1];
+        else ret *= num[i + 1];
+    }
+    return ret;
+}
+
+int dfs(int idx){
+
+    return 0;
 }
 
 int main (void){
+    int ans = 0;
+
     scanf("%d\n",&n);
 
     for(int i = 0; i < n; i++){
         if((i % 2) == 1) scanf("%c",&op[i]);
         else scanf("%d",&num[i]);
     }
+    ans = cal(1);
+
+    for(int i = 0; i < n; i++){
+        ans = max(ans, dfs(i));
+    }
+    
+    printf("%d\n",ans);
 }
