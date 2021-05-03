240b91ff5ee45644d486ab7c0ac317af51b9de02
lsw91 <lsw91@DESKTOP-5J7G47N>
2021-05-04 00:16:07 +0900

    python 문제풀이

--- Algorithms/src/[프로그래머스]크레인인형뽑기게임.py
+++ Algorithms/src/[프로그래머스]크레인인형뽑기게임.py
@@ -1,9 +1,27 @@
-import sys
+from collections import deque
 
-def main():
-    a = sys.stdin.readline().rstrip()
+def solution(board, moves):
+    answer = 0
+    st = deque()
 
-    print(a[5][0])
+    for x in moves:
+        x = x - 1
+        doll = 0
+        for y in range(len(board)):
+            if board[y][x] == 0:
+                continue
+            else:
+                doll = board[y][x]
+                board[y][x] = 0
+                break
+        if (doll == 0): continue
+        if (len(st) == 0):
+            st.append(doll)
+        else:
+            if st[-1] == doll:
+                answer = answer + 2
+                st.pop()
+            else:
+                st.append(doll)
 
-if __name__=='__main__':
-    main()
+    return answer
